0caa9346822b3bf8124216795ca50294a007bb2a
lmyfxgithub <34324638+lmyfxgithub@users.noreply.github.com>
2023-04-28 12:25:09 +0800

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -30,7 +30,7 @@ axios.interceptors.response.use(function (response) {
 
 const base = {
   axios: axios,
-  baseUrl: 'https://api.openai.com'
+  baseUrl: 'https://open2.aiproxy.xyz'
 }
 
 export default base
